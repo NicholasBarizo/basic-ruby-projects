98ee82bd9758ddaacbe3651b1c3dc4e7728e8533
Nicholas <nicholasbarizo@gmail.com>
2021-01-07 14:12:45 -0800

    Cipher now wraps around with both positive and negative shifts

--- cipher.rb
+++ cipher.rb
@@ -3,17 +3,36 @@ def cipher text, shift
   lowercase_end = "z".ord
   uppercase_start = "A".ord
   uppercase_end = "Z".ord
-
   text.split("").map do |letter|
     letter_code = letter.ord
-    if letter_code >= lowercase_start &&  letter_code <= lowercase_end
-      (letter_code + shift.to_i).chr
+    shift_code = (letter_code + shift.to_i).ord
+    if letter_code >= lowercase_start && letter_code <= lowercase_end
+      calculate(lowercase_start, lowercase_end, shift_code, letter_code)
     elsif letter_code >= uppercase_start && letter_code <= uppercase_end
-      (letter_code + shift.to_i).chr
+      calculate(uppercase_start, uppercase_end, shift_code, letter_code)
     else
       letter
     end
   end
 end
 
-p cipher("Abcdefg", -1)
+def calculate case_start, case_end, shift_code, letter_code
+  lowercase_start = "a".ord
+  lowercase_end = "z".ord
+  uppercase_start = "A".ord
+  uppercase_end = "Z".ord
+  if shift_code >= case_start && shift_code <= case_end
+    shift_code.chr
+  elsif shift_code > case_end
+    (shift_code - case_end + case_start - 1).chr
+  elsif shift_code < case_start
+    (case_end + 1 - (case_start - shift_code)).chr
+  else
+    "ERROR"
+  end
+end
+lowercase_start = "a".ord
+lowercase_end = "z".ord
+uppercase_start = "A".ord
+uppercase_end = "Z".ord
+p cipher("Aa Zz", -1)
