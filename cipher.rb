6dbc0a7384801970f91db4843f62b3cf06b19509
Nicholas <nicholasbarizo@gmail.com>
2021-01-08 00:38:25 -0800

    slightly condensed calculate method

--- cipher.rb
+++ cipher.rb
@@ -6,33 +6,32 @@ def cipher text, shift
   uppercase_end = "Z".ord
 
   while shift > 26 do
-    shift -= 26
+    shift %= 26
   end
   while shift < -26 do
-    shift += 26
+    shift %= 26
   end
-
   text.split("").map do |letter|
     letter_code = letter.ord
     shift_code = (letter_code + shift).ord
-    
-    if letter_code >= lowercase_start && letter_code <= lowercase_end
-      calculate(lowercase_start, lowercase_end, shift_code, letter_code)
-    elsif letter_code >= uppercase_start && letter_code <= uppercase_end
-      calculate(uppercase_start, uppercase_end, shift_code, letter_code)
+
+    if letter_code.between?(lowercase_start, lowercase_end)
+      calculate(lowercase_start, lowercase_end, shift_code, letter_code, shift)
+    elsif letter_code.between?(uppercase_start, uppercase_end)
+      calculate(uppercase_start, uppercase_end, shift_code, letter_code, shift)
     else
       letter
     end
   end
 end
 
-def calculate case_start, case_end, shift_code, letter_code
-  if shift_code >= case_start && shift_code <= case_end
+def calculate case_start, case_end, shift_code, letter_code, shift
+  if shift_code.between?(case_start, case_end)
     shift_code.chr
   elsif shift_code > case_end
-    (shift_code - case_end + case_start - 1).chr
+    (shift + case_start - 1).chr
   elsif shift_code < case_start
-    (case_end + 1 - (case_start - shift_code)).chr
+    (case_end - shift - 1).chr
   end
 end
 
