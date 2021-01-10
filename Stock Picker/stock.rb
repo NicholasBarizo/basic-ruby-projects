45228df2268d640273f461b0bf6ac6472c0ac5bf
Nicholas <nicholasbarizo@gmail.com>
2021-01-09 21:29:54 -0800

    determines the lowest buy and highest sell resulting in the most profit while going forward in time

--- Stock Picker/stock.rb
+++ Stock Picker/stock.rb
@@ -1,16 +1,22 @@
 def picker prices
-  buy_sell = [0, 0]
-  prices.reduce(1) do |best_difference, price|
-    buy_sell[1] = prices.reduce(1) { |best_compar, price_compar| price_compar > best_compar ? price_compar : best_compar }
-    if best_difference > buy_sell[1] - price
-      best_difference
-    else
-      buy_sell[0] = price
-      buy_sell[1] - price
+  buy_sell = [prices[0], 0]
+  profit = 0
+  best_profit = 0
+  prices.each_with_index do |buy_price, index|
+    prices.each_with_index do |sell_price, sell_index|
+      next if index >= sell_index
+      if sell_price >= buy_sell[1]
+        buy_sell[1] = sell_price
+        profit = sell_price - buy_price
+      end
+    end
+    if profit > best_profit
+      buy_sell[0] = buy_price
+      best_profit = profit
     end
   end
   buy_sell
 end
-stocks = [8, 6, 3, 8, 3, 14, 8, 6, 9, 6, 5, 11]
 
+stocks = [8, 6, 3, 8, 3, 33, 8, 6, 9, 62, 1, 22]
 p picker(stocks)
